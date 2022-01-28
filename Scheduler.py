40bd6672b5f60c48b6d54bfd38e6495e964a08d1
Amirreza Hosseini <94783019+AmirrezaHosseini@users.noreply.github.com>
2022-01-28 21:02:47 +0330

    Finish commit

--- Scheduler.py
+++ Scheduler.py
@@ -12,9 +12,11 @@ def HRRN(tasks):
     time = 0
 
     print("Start Highest Response Ratio next")
+    print("Ready Queue ")
 
     while len(Qtask) > 0:
         Qtask.sort(key=lambda x: (((time + x[1]) / x[1]), -tasks.index(x)), reverse=True)
+        print("Next Task ")
         while Qtask[0][1] > 0:
             Qtask[0][1] -= 1
             print(str(time) + ': task ' + Qtask[0][0] + ', left overtime : ' + str(Qtask[0][1]))
@@ -25,9 +27,12 @@ def HRRN(tasks):
 def shortestJobFirst(tasks):
     Qtask = copy.deepcopy(tasks)
     time = 0
-    Qtask.sort(k=lambda x: x[1])
+    Qtask.sort(key=lambda x: x[1])
     print("start shortestJobFirst")
+    print("Ready Queue ")
+
     for i in Qtask:
+        print("Next Task ")
         while i[1] > 0:
             i[1] -= 1
             time += 1
@@ -38,8 +43,10 @@ def FCFS(tasks):
     Qtask = copy.deepcopy(tasks)
     s = 0
     print('FCFS Start')
+    print("Ready Queue ")
 
     for i in Qtask:
+        print("Next Task ")
         while i[1] > 0:
             i[1] -= 1
             s += 1
@@ -53,6 +60,8 @@ def RoundRobin(tasks, quantum):
     time = 0
 
     print("Start RoundRobin")
+    print("Ready Queue ")
+
     while len(Qtask) > 0:
         finished = False
         for i in range(quantum):
@@ -79,7 +88,7 @@ if __name__ == '__main__':
         tasks.append(input().split(' '))
         tasks[i][1] = int(tasks[i][1])
 
-    if algorithm == "hhrn":
+    if algorithm == "hrrn":
         HRRN(tasks)
     elif algorithm == "sjf":
         shortestJobFirst(tasks)
